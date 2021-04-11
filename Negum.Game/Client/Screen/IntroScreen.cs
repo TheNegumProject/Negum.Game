9053355ed6b78296e56fd8ff0fb0a14164981f5f
kdobrzynski <k.dobrzynski94@gmail.com>
2021-04-11 13:37:26 +0200

    Started working on basic screens;

--- Negum.Game/Client/Screen/IntroScreen.cs
+++ Negum.Game/Client/Screen/IntroScreen.cs
@@ -3,6 +3,8 @@ using Negum.Game.Client.Input;
 namespace Negum.Game.Client.Screen
 {
     /// <summary>
+    /// Screen which represents Logo / Intro / Opening Video / etc.
+    /// This screen should always set Main Menu Screen as the next one.
     /// </summary>
     /// 
     /// <author>
@@ -10,6 +12,8 @@ namespace Negum.Game.Client.Screen
     /// </author>
     public class IntroScreen : AbstractScreen
     {
+        protected bool SkipIntro { get; set; }
+
         public IntroScreen(INegumClient client) :
             base(client)
         {
@@ -17,17 +21,40 @@ namespace Negum.Game.Client.Screen
 
         public override void Setup(PlayerKeyBinding binding)
         {
-            // TODO: Any key should skip intro
+            binding.A.SetOnClick(this.DoSkip);
+            binding.B.SetOnClick(this.DoSkip);
+            binding.C.SetOnClick(this.DoSkip);
+
+            binding.Crouch.SetOnClick(this.DoSkip);
+            binding.Jump.SetOnClick(this.DoSkip);
+            binding.Left.SetOnClick(this.DoSkip);
+            binding.Right.SetOnClick(this.DoSkip);
+            binding.Start.SetOnClick(this.DoSkip);
+
+            binding.X.SetOnClick(this.DoSkip);
+            binding.Y.SetOnClick(this.DoSkip);
+            binding.Z.SetOnClick(this.DoSkip);
         }
 
         public override void Tick(double deltaTime)
         {
+            if (this.SkipIntro) // TODO: This should also happen when animation / time ends.
+            {
+                this.Client.Screen.NextScreen(new MainMenuScreen(this.Client));
+                return;
+            }
+
             // TODO: Update intro frame (???)
         }
 
         public override void Render()
         {
             // TODO: Render intro OR skip directly if none intro provided
+        }
+
+        protected virtual void DoSkip()
+        {
+            this.SkipIntro = true;
         }
     }
 }
---
+++ Negum.Game/Client/Screen/MainMenuScreen.cs
@@ -0,0 +1,31 @@
+using Negum.Game.Client.Input;
+
+namespace Negum.Game.Client.Screen
+{
+    /// <summary>
+    /// Screen which represents Main Menu.
+    /// </summary>
+    /// 
+    /// <author>
+    /// https://github.com/TheNegumProject/Negum.Game
+    /// </author>
+    public class MainMenuScreen : AbstractScreen
+    {
+        public MainMenuScreen(INegumClient client) :
+            base(client)
+        {
+        }
+
+        public override void Setup(PlayerKeyBinding binding)
+        {
+        }
+
+        public override void Tick(double deltaTime)
+        {
+        }
+
+        public override void Render()
+        {
+        }
+    }
+}
